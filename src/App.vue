b3a9881ad900ee291f2385ff154ef09c01710e81
Sebastián Gómez Letelier <sgl1994@gmail.com>
2021-08-23 23:27:43 -0400

    first commit tras una semana entendiendo

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+
+<div id="Morty">
+  <h2>Lista de personajes de Rick and Morty</h2>
+  <hr>
+
+  <div>
+    <PersonajesMorty/>
+    
+    
+  </div>
+  
+            
+        </div>
+  
+</template>
+
+<script>
+
+import PersonajesMorty from './components/mortyImages.vue'
+//import image from './components/mortyImages.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PersonajesMorty,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: greenyellow;
+  margin-top: 60px;
+}
+body {
+    background-image: url(https://rickandmortypod.com/wp-content/uploads/2018/11/cropped-RM_page-header_background1-3.png);
+    z-index: 0;
+    color: greenyellow;
+};
+</style>
---
+++ src/components/mortyImages.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        
+        <div id="Morty" v-for="personaje in personajes" 
+        :key="personaje.name" 
+        style="display:flex; align-items: center">    
+            <img :src="personaje.image" 
+            style="width:20%;
+            border-radius: 50%;
+            padding: 15pt " 
+            alt="">
+            <h2>{{personaje.name}}</h2>
+            
+        </div>
+        <div v-for="personaje in personajes" :key="personaje.image">
+            
+        </div>
+
+    </div>
+</template>
+
+<script>
+//import image from "https://rickandmortyapi.com/api/character"
+
+export default {
+    name: "PersonajesMorty",
+    data: () => ({
+        
+        personajes:[
+            {
+                name: "Rick Sanchez",
+                image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+            },
+            {
+                name: "Morty Smith",
+                image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+            },
+            {
+                name: "Summer Smith",
+                image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
+            },
+            {
+                name: "Beth Smith",
+                image: "https://rickandmortyapi.com/api/character/avatar/4.jpeg"
+            },
+            {
+                name: "Jerry Smith",
+                image: "https://rickandmortyapi.com/api/character/avatar/5.jpeg"
+            },
+            
+        ],
+    })
+
+}
+</script>
+
+<style scoped>
+
+
+</style>
